a0e43cbe1e911b5ca18dcdce9f9b0862cc95910a
AwaisFazli <awaisfazli947@gmail.com>
2024-11-11 20:22:00 +0500

    authentication added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,24 @@
+import Rails from '@rails/ujs';
+Rails.start();
+
 console.log("Hello, Rails 7 with JavaScript!");
 
-const toggle = document.getElementById("language-dropdown-toggle");
-const dropdown = document.getElementById("language-dropdown");
 
-toggle.addEventListener("click", function (event) {
-  event.preventDefault();
-  dropdown.classList.toggle("hidden");
-});
+  const toggle = document.getElementById("language-dropdown-toggle");
+  const dropdown = document.getElementById("language-dropdown");
 
-// Close the dropdown if clicked outside of it
-document.addEventListener("click", function (event) {
-  if (!toggle.contains(event.target) && !dropdown.contains(event.target)) {
-    dropdown.classList.add("hidden");
+  // Only add event listeners if the elements exist
+  if (toggle && dropdown) {
+    // Toggle the dropdown visibility on click
+    toggle.addEventListener("click", function (event) {
+      event.preventDefault();
+      dropdown.classList.toggle("hidden");
+    });
+
+    // Close the dropdown if clicked outside of it
+    document.addEventListener("click", function (event) {
+      if (!toggle.contains(event.target) && !dropdown.contains(event.target)) {
+        dropdown.classList.add("hidden");
+      }
+    });
   }
-});
